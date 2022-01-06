385f972c46afcfff312e6dbcd390baa42ec1d766
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-01-05 18:18:08 -0800

    font settings

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Ubuntu Mono:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Ubuntu Mono Nerd Font:pixelsize=20:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
